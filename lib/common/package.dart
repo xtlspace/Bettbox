0599a5cf2152818407f2191316e49a612ac94610
xtlspace <souloutclub@gmail.com>
2026-04-28 13:43:28 +0800

    Update package.dart
    修改默认UA

--- lib/common/package.dart
+++ lib/common/package.dart
@@ -2,5 +2,5 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 extension PackageInfoExtension on PackageInfo {
   String get ua =>
-      ['Clash.Meta/ClashMetaForAndroid/5.0'].join(' ');
+      ['ILoveU-Meta'].join(' ');
 }
